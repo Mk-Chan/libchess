a88f3d449fa68940716d79d7f8485e210d43b8dc
Manik Charan <mkchan2951@gmail.com>
2019-06-15 02:21:49 +0530

    Faster and stable MoveList.sort
    The semantics of the function were changed. Now the function takes in a
    move evaluation function with signature `int func_name(Move)`.

--- Move.h
+++ Move.h
@@ -74,7 +74,7 @@ class Move {
 };
 
 class MoveList {
-  private:
+  protected:
     constexpr static inline int max_size = 256;
 
   public:
@@ -94,8 +94,27 @@ class MoveList {
         values_[size()] = move;
         ++size_;
     }
-    template <class F> constexpr inline void sort(F comparator) {
-        std::sort(begin(), end(), comparator);
+    template <class F> constexpr inline void sort(F move_evaluator) {
+        auto& moves = values_mut_ref();
+        std::array<int, max_size> scores{};
+        for (int i = 0; i < size(); ++i) {
+            scores[i] = move_evaluator(moves[i]);
+        }
+        for (int i = 1; i < size(); ++i) {
+            Move moving_move = moves[i];
+            int moving_score = scores[i];
+            int j = i;
+            for (; j > 0; --j) {
+                if (scores[j - 1] < moving_score) {
+                    scores[j] = scores[j - 1];
+                    moves[j] = moves[j - 1];
+                } else {
+                    break;
+                }
+            }
+            scores[j] = moving_score;
+            moves[j] = moving_move;
+        }
     }
     constexpr inline int size() const { return size_; }
     constexpr inline const std::array<Move, max_size>& values() const { return values_; }
